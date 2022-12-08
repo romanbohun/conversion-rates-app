27788413818f3cedc619d656fc91c09e294e6c4f
Roman Bohun <roman.v.bogun@hotmail.com>
2022-12-08 20:12:43 +0100

    Init empty project

---
+++ ConversionRates/ConversionRates/ConversionRatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConversionRatesApp.swift
+//  ConversionRates
+//
+//  Created by Roman Bogun on 08.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ConversionRatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
